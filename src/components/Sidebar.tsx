fb00b8aa6e0d04dc68dc0cd1d9e49284cc9d17b5
Giorgi Solomonidze <solomonidze29@gmail.com>
2026-01-27 00:44:09 +0400

    update build

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -147,7 +147,7 @@ export function Sidebar({ onSelectTable, onNewConnection }: SidebarProps) {
         isLoading={isDeleting}
       />
 
-      <div className="p-2 text-[10px] text-gray-400 border-t border-gray-200 dark:border-gray-700 text-center">Build: 8</div>
+      <div className="p-2 text-[10px] text-gray-400 border-t border-gray-200 dark:border-gray-700 text-center">Build: 9</div>
     </div>
   );
 }
